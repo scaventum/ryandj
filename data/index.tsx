63f2078e8d34822583aa440edd7a1eda37e0f145
Ryan DJ <ryan.djoenaidi@akqa.com>
2023-11-29 18:32:25 +0700

    Display static data

---
+++ data/index.tsx
@@ -0,0 +1,418 @@
+const data = {
+  profile: {
+    name: "Ryan Djoenaidi",
+    role: "Senior developer",
+    introduction:
+      "NZ-based senior developer proficient in PHP (Laravel) and JavaScript (React), experienced in AWS and Azure infrastructure management. Seeking opportunities to advance leadership skills and stay updated on evolving development technologies.",
+  },
+  skills: {
+    highlighted: [
+      {
+        icon: {
+          name: "laravel",
+          style: "brands",
+        },
+        label: "Laravel",
+        score: 9,
+      },
+      {
+        icon: {
+          name: "php",
+          style: "brands",
+        },
+        label: "PHP",
+        score: 8,
+      },
+      {
+        icon: {
+          name: "react",
+          style: "brands",
+        },
+        label: "React",
+        score: 7,
+      },
+      {
+        icon: {
+          name: "js",
+          style: "brands",
+        },
+        label: "JS",
+        score: 7,
+      },
+      {
+        icon: {
+          name: "aws",
+          style: "brands",
+        },
+        label: "AWS",
+        score: 7,
+      },
+    ],
+    categorised: [
+      {
+        icon: {
+          name: "server",
+        },
+        category: "back-end",
+        label: "Back end",
+        items: [
+          {
+            icon: {
+              name: "laravel",
+              style: "brands",
+            },
+            label: "Laravel",
+            score: 9,
+          },
+          {
+            icon: {
+              name: "php",
+              style: "brands",
+            },
+            label: "PHP",
+            score: 8,
+          },
+          {
+            icon: {
+              name: "database",
+            },
+            label: "MySQL",
+            score: 9,
+          },
+          {
+            icon: {
+              name: "cube",
+            },
+            label: "Composer",
+            score: 7,
+          },
+          {
+            icon: {
+              name: "terminal",
+            },
+            label: "Artisan",
+            score: 8,
+          },
+          {
+            icon: {
+              name: "server",
+            },
+            label: "NGINX",
+            score: 6,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "window-maximize",
+        },
+        category: "front-end",
+        label: "Front end",
+        items: [
+          {
+            icon: {
+              name: "react",
+              style: "brands",
+            },
+            label: "React",
+            score: 7,
+          },
+          {
+            icon: {
+              name: "js",
+              style: "brands",
+            },
+            label: "JS",
+            score: 7,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "cloud",
+        },
+        category: "infrastructure",
+        label: "Infrastructures",
+        items: [
+          {
+            icon: {
+              name: "aws",
+              style: "brands",
+            },
+            label: "AWS",
+            score: 7,
+          },
+          {
+            icon: {
+              name: "microsoft",
+              style: "brands",
+            },
+            label: "Azure",
+            score: 6,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "gears",
+        },
+        category: "operation-system",
+        label: "Operation Systems",
+        items: [
+          {
+            icon: {
+              name: "ubuntu",
+              style: "brands",
+            },
+            label: "Linux",
+            score: 6,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "list-check",
+        },
+        category: "management",
+        label: "Management",
+        items: [
+          {
+            icon: {
+              name: "jira",
+              style: "brands",
+            },
+            label: "Jira",
+            score: 8,
+          },
+          {
+            icon: {
+              name: "confluence",
+              style: "brands",
+            },
+            label: "Confluence",
+            score: 8,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "arrows-spin",
+        },
+        category: "workflow",
+        label: "Workflow",
+        items: [
+          {
+            icon: {
+              name: "git",
+              style: "brands",
+            },
+            label: "git",
+            score: 8,
+          },
+          {
+            icon: {
+              name: "bitbucket",
+              style: "brands",
+            },
+            label: "Bitbucket",
+            score: 7,
+          },
+          {
+            icon: {
+              name: "github",
+              style: "brands",
+            },
+            label: "GitHub",
+            score: 6,
+          },
+        ],
+      },
+      {
+        icon: {
+          name: "list",
+        },
+        category: "other",
+        label: "Other",
+        items: [
+          {
+            icon: {
+              name: "code",
+            },
+            label: "SilverStripe",
+            score: 5,
+          },
+          {
+            icon: {
+              name: "code",
+            },
+            label: "CodeIgniter",
+            score: 5,
+          },
+          {
+            icon: {
+              name: "vuejs",
+              style: "brands",
+            },
+            label: "Vue",
+            score: 5,
+          },
+          {
+            icon: {
+              name: "gitlab",
+              style: "brands",
+            },
+            label: "GitLab",
+            score: 5,
+          },
+        ],
+      },
+    ],
+  },
+  experiences: [
+    {
+      organisationName: "AKQA",
+      location: "Wellington - New Zealand",
+      period: {
+        from: "Sep 2019",
+        to: "Ongoing",
+      },
+      jobDescription: [
+        "Collaborating closely with other full-stack developers, clients, product owners, third parties, and designers to deliver high-quality products in an agile environment",
+        "Coordinating and assigning work within sprints and quarterly cycles in close collaboration with other developers to achieve targets.",
+        "Intensively involved in discovering, developing, and delivering new features, applications, and infrastructures.",
+        "Maintaining and servicing applications and infrastructure.",
+      ],
+    },
+    {
+      organisationName: "Dream Config",
+      location: "Auckland - New Zealand",
+      period: {
+        from: "Jan 2019",
+        to: "Aug 2019",
+      },
+      jobDescription: [
+        "Working closely with developer, designer, and director to meet client requirements collaboratively.",
+        "Organizing and delegating tasks within daily cycles to meet set targets.",
+        "Guiding the development approach across front-end, back-end, and infrastructure.",
+        "Maintaining and servicing applications and infrastructure.",
+      ],
+    },
+    {
+      organisationName: "SkyHi",
+      location: "Auckland - New Zealand",
+      period: {
+        from: "Aug 2018",
+        to: "Dec 2018",
+      },
+      jobDescription: [
+        "Working closely with director to meet client requirements.",
+        "Organizing and delegating tasks within daily cycles to meet set targets.",
+        "Maintaining and servicing applications and infrastructure.",
+      ],
+    },
+    {
+      organisationName: "Rinnai Indonesia",
+      location: "Jakarta - Indonesia",
+      period: {
+        from: "Oct 2014",
+        to: "May 2017",
+      },
+      jobDescription: [
+        "Engaging closely with full-stack developers, clients, and product owners to deliver features and fixes.",
+        "Organizing and delegating tasks within daily cycles to meet set targets.",
+        "Guiding the development approach across front-end, back-end, and infrastructure of web application.",
+        "Maintaining and servicing applications and infrastructure.",
+      ],
+    },
+    {
+      organisationName: "Pro-Int Dinamika",
+      location: "Jakarta - Indonesia",
+      period: {
+        from: "Aug 2013",
+        to: "Sep 2014",
+      },
+      jobDescription: [
+        "Working closely with senior developers and system analysts to deliver features and fixes.",
+        "Organizing and delegating tasks within daily cycles to meet set targets.",
+        "Maintaining and servicing applications.",
+      ],
+    },
+  ],
+  educations: [
+    {
+      degree: "Bachelor of IT",
+      organisationName: "WelTec",
+      location: "Lower Hutt - New Zealand",
+      period: {
+        from: "July 2017",
+        to: "June 2018",
+      },
+    },
+    {
+      degree: "Bachelor of IT",
+      organisationName: "Universitas Tarumanagara",
+      location: "Jakarta - Indonesia",
+      period: {
+        from: "August 2009",
+        to: "July 2013",
+      },
+    },
+  ],
+  certifications: [
+    {
+      title: "edX Verified Certificate for AI for Everyone: Master the Basics",
+      link: "https://courses.edx.org/certificates/b65a31b3da03458da62b1097fba28cac",
+      issuedDate: "July 2023",
+      issuedBy: "IBM",
+    },
+    {
+      title: "AWS Certified Solutions Architect - Associate",
+      link: "https://www.credly.com/badges/674de27d-9341-4de4-9eee-f764b3f354f6",
+      issuedDate: "September 2020",
+      issuedBy: "AWS",
+    },
+  ],
+  contacts: [
+    {
+      icon: {
+        name: "linkedin",
+        style: "brands",
+      },
+      label: "LinkedIn",
+      value: "https://www.linkedin.com/in/ryan-djoenaidi-383b96166/",
+    },
+    {
+      icon: {
+        name: "gmail",
+        style: "brands",
+      },
+      label: "Gmail",
+      value: "mailto:ryan.djoenaidi@gmail.com",
+    },
+    {
+      icon: {
+        name: "whatsapp",
+        style: "brands",
+      },
+      label: "WhatsApp",
+      value: "https://wa.me/622040995746",
+    },
+    {
+      icon: {
+        name: "github",
+        style: "brands",
+      },
+      label: "GitHub",
+      value: "https://github.com/scaventum",
+    },
+  ],
+  hobbies: [
+    "Programming, drawing, designing",
+    "Table tennis, Basketball, Badminton",
+    "YouTube, Netflix",
+    "Tramping",
+  ],
+  referees: ["Available upon request."],
+};
+
+export default data;
